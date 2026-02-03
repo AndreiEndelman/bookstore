4060e5ab1aa985748fa11b5d1e5097211765221e
AndreiEndelman <andreiendelman@hotmail.com>
2026-02-02 20:57:54 -0500

    Added sample data with mongoDB integration

--- src/main/java/com/bookstore/rest_api_project/RestApiProjectApplication.java
+++ src/main/java/com/bookstore/rest_api_project/RestApiProjectApplication.java
@@ -2,13 +2,28 @@ package com.bookstore.rest_api_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import com.bookstore.rest_api_project.model.Book;
+import com.bookstore.rest_api_project.repository.BookRepository;
 
 @SpringBootApplication
 public class RestApiProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApiProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiProjectApplication.class, args);
+    }
 
+    // ✅ This is the new part
+    @Bean
+    CommandLineRunner testMongo(BookRepository repo) {
+        return args -> {
+            // Add a sample book to the DB
+            repo.save(new Book("1", "Clean Code", "Robert Martin"));
+
+            // Print all books in the DB
+            System.out.println("Books in DB: " + repo.findAll());
+        };
+    }
 }
-//skeleton setup complete
