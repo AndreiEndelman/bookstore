0eab1d369ad3d925ead8350307437c8d5b4e1893
Luis <leondorado04@gmail.com>
2026-02-17 15:33:26 -0500

    added genre setter in Book model

--- src/main/java/com/bookstore/rest_api_project/model/Book.java
+++ src/main/java/com/bookstore/rest_api_project/model/Book.java
@@ -9,13 +9,15 @@ public class Book {
     private String id;
     private String title;
     private String author;
+    private String genre;
 
     public Book() {} // default constructor
 
-    public Book(String id, String title, String author) {
+    public Book(String id, String title, String author, String genre) {
         this.id = id;
         this.title = title;
         this.author = author;
+        this.genre = genre;
     }
 
     // getters and setters
@@ -25,4 +27,6 @@ public class Book {
     public void setTitle(String title) { this.title = title; }
     public String getAuthor() { return author; }
     public void setAuthor(String author) { this.author = author; }
+    public String getGenre() { return genre; }
+    public void setGenre(String author) { this.genre = genre; }
 }
