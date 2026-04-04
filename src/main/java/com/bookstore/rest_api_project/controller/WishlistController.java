a47be975efbc6b39cd50f9cee11e3acd04953e89
unknown <rudealpacagaming@gmail.com>
2026-04-04 10:16:10 -0400

    Complete wishlist features: create, add book, remove book, list books, delete wishlist(To clean up a little)

--- src/main/java/com/bookstore/rest_api_project/controller/WishlistController.java
+++ src/main/java/com/bookstore/rest_api_project/controller/WishlistController.java
@@ -1,5 +1,6 @@
 package com.bookstore.rest_api_project.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,14 +9,20 @@ import org.springframework.web.bind.annotation.*;
 
 import com.bookstore.rest_api_project.model.Wishlist;
 import com.bookstore.rest_api_project.repository.WishlistRepository;
+import com.bookstore.rest_api_project.model.Book;
+import com.bookstore.rest_api_project.repository.BookRepository;
+
 
 @RestController
 public class WishlistController {
 
+    private final BookRepository bookRepo;
     private final WishlistRepository repo;
+    
 
-    public WishlistController(WishlistRepository repo) {
+    public WishlistController(WishlistRepository repo, BookRepository bookRepo) {
         this.repo = repo;
+        this.bookRepo = bookRepo;
     }
 
     @GetMapping("/wishlist-test")
@@ -58,4 +65,52 @@ public Wishlist getWishlistById(@PathVariable String id) {
 
         repo.save(wishlist);
     }
+
+
+    @DeleteMapping("/wishlists/remove-book")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeBookFromWishlist(@RequestBody Map<String, String> request) {
+        String wishlistId = request.get("wishlistId");
+        String bookId = request.get("bookId");
+
+        Wishlist wishlist = repo.findById(wishlistId).orElse(null);
+
+        if (wishlist == null) {
+            throw new RuntimeException("Wishlist not found.");
+        }
+
+       wishlist.getBookIds().remove(bookId);
+
+        repo.save(wishlist);
+    }
+
+    @GetMapping("/wishlists/books/{wishlistId}")
+    public List<Book> getBooksInWishlist(@PathVariable String wishlistId) {
+        Wishlist wishlist = repo.findById(wishlistId).orElse(null);
+
+        
+
+        if (wishlist == null) {
+            throw new RuntimeException("Wishlist not found.");
+        }
+
+        if(wishlist.getBookIds() == null || wishlist.getBookIds().isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return bookRepo.findAllById(wishlist.getBookIds());
+    }
+
+    @DeleteMapping("/wishlists/{id}")
+@ResponseStatus(HttpStatus.OK)
+public void deleteWishlistById(@PathVariable String id) {
+    Wishlist wishlist = repo.findById(id).orElse(null);
+
+    if (wishlist == null) {
+        throw new RuntimeException("Wishlist not found.");
+    }
+
+    repo.deleteById(id);
+}
+    
 }
